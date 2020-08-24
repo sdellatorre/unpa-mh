7164899116d8da7504b1d616ce75490aaa612cae
Santiago Della Torre <sadellatorre@issys.gov.ar>
2020-08-24 14:03:35 -0300

    Se corrigen nombres campos

--- ga/salida/schema.sql
+++ ga/salida/schema.sql
@@ -1,10 +1,12 @@
+BEGIN;
+
 CREATE TEMP TABLE tmp_ga (
 	funcion text,
 	pc text,
 	pm text,
-	e text,
+	elitismo text,
 	sel text,
-	r text,
+	reempl text,
 	sem int,
 	val numeric,
 	x text,
@@ -15,3 +17,20 @@ CREATE TEMP TABLE tmp_ga (
 \copy tmp_ga from detalle_f1.txt 
 	
 	
+UPDATE tmp_ga SET elitismo = 'CON ELITISMO' WHERE elitismo = '1';
+UPDATE tmp_ga SET elitismo = 'SIN ELITISMO' WHERE elitismo = '0';
+
+UPDATE tmp_ga SET reempl = 'REEMPLAZO GENERACIONAL' WHERE reempl = '1';
+UPDATE tmp_ga SET reempl = 'REEMPLAZO SS' WHERE reempl = '2';
+
+
+UPDATE tmp_ga SET sel = 'RULETA' WHERE sel = '1';
+UPDATE tmp_ga SET sel = 'TORNEO' WHERE sel = '2';
+
+
+UPDATE tmp_ga SET pm = 'PM = 0.01' WHERE pm = '01';
+UPDATE tmp_ga SET pm = 'PM = 0.0'  WHERE pm = '0';
+UPDATE tmp_ga SET pm = 'PM = 0.1'  WHERE pm = '1';
+
+UPDATE tmp_ga SET pc = 'PC = 0.' || pc;
+END;
